ca285497a21b34d0d569b94c619c468ecd9b1f14
gary318 <gary318@126.com>
2025-02-21 15:38:12 +0800

    Feat: Implement a basic task allocation model

--- agent.py
+++ agent.py
@@ -15,6 +15,26 @@ class RelicInfo(Enum):
     FAKE = 0
     REAL = 1
 
+class UnitTaskType(Enum):
+    """单位任务类型"""
+    DEAD = -1
+    NOT_ALLOCATED = 0
+    CAPTURE_RELIC = 1
+    EXPLORE_RELIC = 2
+    WANDER = 3
+
+class UnitTask:
+    """单位任务"""
+
+    type: UnitTaskType
+    target_pos: np.ndarray
+    start_step: int
+
+    def __init__(self, type: UnitTaskType, target_pos: np.ndarray, start_step: int) -> None:
+        self.type = type
+        self.target_pos = target_pos
+        self.start_step = start_step
+
 class Agent():
 
     player: str
@@ -38,6 +58,9 @@ class Agent():
     obs: Observation
     """当前观测结果"""
 
+    task_list: List[UnitTask]
+    """单位任务列表"""
+
     logger: Logger = Logger()
 
     def __init__(self, player: str, env_cfg: Dict[str, Any], obs: Observation) -> None:
@@ -51,6 +74,7 @@ class Agent():
         self.relic_center = np.zeros((0, 2), dtype=np.int8)
         self.relic_nodes = np.zeros((0, C.RELIC_SPREAD*2+1, C.RELIC_SPREAD*2+1), dtype=np.int8)
 
+        self.task_list = [UnitTask(UnitTaskType.NOT_ALLOCATED, np.zeros(2, dtype=np.int8), 0) for _ in range(C.MAX_UNITS)]
         self.relic_node_positions = []
         self.discovered_relic_nodes_ids = set()
         self.unit_explore_locations = dict()
@@ -59,10 +83,7 @@ class Agent():
         self.history = []
 
     def act(self, step: int, obs: Observation, remainingOverageTime: int = 60) -> np.ndarray:
-        """implement this function to decide what actions to send to each available unit.
-
-        step is the current timestep number of the game starting from 0 going up to max_steps_in_match * match_count_per_episode - 1.
-        """
+        """step is the current timestep number of the game starting from 0 going up to max_steps_in_match * match_count_per_episode - 1"""
 
         self.obs = obs
         self.logger.set_step(step)
@@ -70,6 +91,9 @@ class Agent():
         # 更新地图并进行参数估计
         self.game_map.update_map(obs)
         self.estimate_params()
+
+        if step == 0:
+            return np.zeros((C.MAX_UNITS, 3), dtype=int)
 
         # 添加新发现的遗迹中心点位置
         # TODO: 新的遗迹范围若与旧的重合, 则重合部分的FAKE应该重新设为UNKNOWN
@@ -83,58 +107,74 @@ class Agent():
         # 估计遗迹得分点的位置
         relic_updated = self.estimate_relic_positions()
         self.update_relic_map()
-        if relic_updated:
-            self.logger.info(f"Map updated: \n{self.relic_map.T}")
+        # if relic_updated:
+        #     self.logger.info(f"Map updated: \n{self.relic_map.T}")
 
-        unit_mask = obs.units_mask[self.team_id]  # shape (max_units, )
-        unit_positions = obs.units_position[self.team_id]  # shape (max_units, 2)
-        unit_energys = obs.units_energy[self.team_id]  # shape (max_units, 1)
-        observed_relic_node_positions = obs.relic_nodes  # shape (max_relic_nodes, 2)
-        observed_relic_nodes_mask = obs.relic_nodes_mask  # shape (max_relic_nodes, )
+        # 主要策略
+        actions = np.zeros((C.MAX_UNITS, 3), dtype=int)
+        allocated_relic_positions: Set[Tuple[int, int]] = \
+            set([(t.target_pos[0], t.target_pos[1]) for t in self.task_list if t.type == UnitTaskType.CAPTURE_RELIC])
+        real_points = np.where(self.relic_map == RelicInfo.REAL.value)
+        for uid in range(C.MAX_UNITS):
+            u_selector = (self.team_id, uid)
+            if not obs.units_mask[u_selector]:  # 死亡单位不做任何动作
+                self.task_list[uid] = UnitTask(UnitTaskType.DEAD, np.zeros(2, dtype=np.int8), step)
+                continue
+            elif self.task_list[uid].type == UnitTaskType.DEAD:  # 重新分配任务
+                self.task_list[uid] = UnitTask(UnitTaskType.NOT_ALLOCATED, np.zeros(2, dtype=np.int8), step)
 
-        # ids of units you can control at this timestep
-        available_unit_ids = np.where(unit_mask)[0]
-        # visible relic nodes
-        visible_relic_node_ids = set(np.where(observed_relic_nodes_mask)[0])
+            task = self.task_list[uid]
+            u_pos = obs.units_position[u_selector]
+            u_energy = obs.units_energy[u_selector]
+            energy_weight = self.energy_weight_fn(u_energy, self.game_map.move_cost)
 
-        actions = np.zeros((self.env_cfg["max_units"], 3), dtype=int)
+            # 任务分配
+            # 先尝试走向一个还没被分配的REAL点
+            if task.type == UnitTaskType.NOT_ALLOCATED or task.type == UnitTaskType.EXPLORE_RELIC:
+                for real_pos in zip(*real_points):
+                    if (real_pos[0], real_pos[1]) not in allocated_relic_positions:
+                        task.type = UnitTaskType.CAPTURE_RELIC
+                        task.target_pos = real_pos
+                        task.start_step = step
+                        allocated_relic_positions.add((real_pos[0], real_pos[1]))
+                        self.logger.info(f"Unit {uid} allocated to capture relic at {real_pos}")
+                        break
+            # 再尝试寻找REAL点
+            if task.type == UnitTaskType.NOT_ALLOCATED and self.relic_center.shape[0] > 0:
+                # 寻找距离最近的遗迹中心点
+                dists = np.sum(np.abs(u_pos - self.relic_center), axis=1)
+                closest_center_idx = np.argmin(dists)
+                closest_center = self.relic_center[closest_center_idx]
+                # 尝试走向遗迹中心点
+                task.type = UnitTaskType.EXPLORE_RELIC
+                task.target_pos = closest_center
+                task.start_step = step
+                self.logger.info(f"Unit {uid} allocated to explore relic center at {closest_center}")
+            # 最后尝试随机游走
+            if task.type == UnitTaskType.NOT_ALLOCATED:
+                task.type = UnitTaskType.WANDER
+                task.target_pos = np.array([np.random.randint(0, C.MAP_SIZE), np.random.randint(0, C.MAP_SIZE)])
+                task.start_step = step
+                self.logger.info(f"Unit {uid} allocated to wander at {task.target_pos}")
 
 
-        # basic strategy here is simply to have some units randomly explore and some units collecting as much energy as possible
-        # and once a relic node is found, we send all units to move randomly around the first relic node to gain points
-        # and information about where relic nodes are found are saved for the next match
-
-        # save any new relic nodes that we discover for the rest of the game.
-        for id in visible_relic_node_ids:
-            if id not in self.discovered_relic_nodes_ids:
-                self.discovered_relic_nodes_ids.add(id)
-                self.relic_node_positions.append(observed_relic_node_positions[id])
-
-
-        # unit ids range from 0 to max_units - 1
-        for unit_id in available_unit_ids:
-            unit_pos = unit_positions[unit_id]
-            unit_energy = unit_energys[unit_id]
-
-            energy_weight = self.energy_weight_fn(unit_energy, self.game_map.move_cost)
-
-            if len(self.relic_node_positions) > 0:
-                nearest_relic_node_position = self.relic_node_positions[0]
-                manhattan_distance = abs(unit_pos[0] - nearest_relic_node_position[0]) + abs(unit_pos[1] - nearest_relic_node_position[1])
-
+            # 任务执行
+            if task.type == UnitTaskType.CAPTURE_RELIC:
+                actions[uid] = [self.game_map.direction_to(u_pos, task.target_pos, energy_weight), 0, 0]
+            elif task.type == UnitTaskType.EXPLORE_RELIC:
+                manhattan_distance = np.sum(np.abs(u_pos - task.target_pos))
                 # if close to the relic node we want to hover around it and hope to gain points
                 if manhattan_distance <= 4:
                     random_direction = np.random.randint(0, 5)
-                    actions[unit_id] = [random_direction, 0, 0]
+                    actions[uid] = [random_direction, 0, 0]
                 else:
                     # otherwise we want to move towards the relic node
-                    actions[unit_id] = [self.game_map.direction_to(unit_pos, nearest_relic_node_position, energy_weight), 0, 0]
-            else:
-                # randomly explore by picking a random location on the map and moving there for about 20 steps
-                if step % 20 == 0 or unit_id not in self.unit_explore_locations:
-                    rand_loc = (np.random.randint(0, self.env_cfg["map_width"]), np.random.randint(0, self.env_cfg["map_height"]))
-                    self.unit_explore_locations[unit_id] = rand_loc
-                actions[unit_id] = [self.game_map.direction_to(unit_pos, self.unit_explore_locations[unit_id], energy_weight), 0, 0]
+                    actions[uid] = [self.game_map.direction_to(u_pos, task.target_pos, energy_weight), 0, 0]
+            elif task.type == UnitTaskType.WANDER:
+                if np.array_equal(u_pos, task.target_pos):
+                    task.type = UnitTaskType.NOT_ALLOCATED
+                else:
+                    actions[uid] = [self.game_map.direction_to(u_pos, task.target_pos, energy_weight), 0, 0]
 
         # save the current observation for the next step
         self.history.append(obs)
