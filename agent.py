2a0e3372e05fa74d0c02e13f5a79709f5217c009
gary318 <gary318@126.com>
2025-02-21 11:49:18 +0800

    Feat: Implement relic position estimation

--- agent.py
+++ agent.py
@@ -1,12 +1,19 @@
-import sys
 import numpy as np
+from enum import Enum
 
+import utils
 from logger import Logger
 from map import Map, Landscape
 from utils import Constants as C
 from observation import Observation
 
-from typing import Dict, List, Any
+from typing import Dict, List, Tuple, Any
+
+class RelicInfo(Enum):
+    """遗迹得分点推断情况"""
+    UNKNOWN = -1
+    FAKE = 0
+    REAL = 1
 
 class Agent():
 
@@ -17,6 +24,14 @@ class Agent():
     env_cfg: Dict[str, Any]
 
     game_map: Map
+    """游戏地图"""
+    relic_center: np.ndarray
+    """可见的遗迹中心点位置, 形状(N, 2)"""
+    relic_nodes: np.ndarray
+    """遗迹得分点推断情况, 数值依照`RelicInfo`枚举设置, 注意平移了RELIC_SPREAD距离
+       形状(N, RELIC_SPREAD*2+1, RELIC_SPREAD*2+1)"""
+    relic_map: np.ndarray
+    """遗迹得分点地图, 在`relic_nodes`更新后更新, 形状(MAP_SIZE, MAP_SIZE)"""
 
     history: List[Observation]
     """历史观测结果, 保存至**上一个回合**"""
@@ -32,6 +47,9 @@ class Agent():
         self.opp_team_id = 1 if self.team_id == 0 else 0
         np.random.seed(0)
         self.env_cfg = env_cfg
+
+        self.relic_center = np.zeros((0, 2), dtype=np.int8)
+        self.relic_nodes = np.zeros((0, C.RELIC_SPREAD*2+1, C.RELIC_SPREAD*2+1), dtype=np.int8)
 
         self.relic_node_positions = []
         self.discovered_relic_nodes_ids = set()
@@ -49,9 +67,20 @@ class Agent():
         self.obs = obs
         self.logger.set_step(step)
 
-        # update map with new observation
+        # 更新地图并进行参数估计
         self.game_map.update_map(obs)
         self.estimate_params()
+
+        # 添加新发现的遗迹中心点位置
+        for r_center in obs.relic_nodes[obs.relic_nodes_mask]:
+            if not np.any(np.all(self.relic_center == r_center, axis=1)):
+                self.logger.info(f"New relic center: {r_center}")
+                self.relic_center = np.vstack((self.relic_center, r_center, utils.flip_coord(r_center)))  # 对称地添加
+                self.relic_nodes = np.vstack((self.relic_nodes,
+                                              np.full((2, C.RELIC_SPREAD*2+1, C.RELIC_SPREAD*2+1), RelicInfo.UNKNOWN.value)))
+
+        # 估计遗迹得分点的位置
+        relic_updated = self.estimate_relic_positions()
 
         unit_mask = obs.units_mask[self.team_id]  # shape (max_units, )
         unit_positions = obs.units_position[self.team_id]  # shape (max_units, 2)
@@ -127,7 +156,7 @@ class Agent():
                         gmap.move_cost = last_obs.units_energy[unit_sel] - self.obs.units_energy[unit_sel] \
                             + gmap.energy_map[tuple(curr_pos)]
                         gmap.move_cost_estimated = True
-                        print(f"Move cost estimated, cost: {gmap.move_cost}", file=sys.stderr)
+                        self.logger.info(f"Move cost estimated: {gmap.move_cost}")
 
         # 估计移动开销后, 估计星云能量损耗
         elif not gmap.nebula_cost_estimated:
@@ -141,7 +170,54 @@ class Agent():
                         gmap.nebula_cost = last_obs.units_energy[unit_sel] - self.obs.units_energy[unit_sel] \
                             + gmap.energy_map[tuple(curr_pos)] - gmap.move_cost
                         gmap.nebula_cost_estimated = True
-                        print(f"Nebula cost estimated, cost: {gmap.nebula_cost}", file=sys.stderr)
+                        self.logger.info(f"Nebula cost estimated: {gmap.nebula_cost}")
+
+    def estimate_relic_positions(self) -> bool:
+        """估计遗迹得分点的位置, 返回是否有更新"""
+        if not len(self.history) or not len(self.relic_center):
+            return False
+
+        delta_pts = self.obs.team_points[self.team_id] - self.history[-1].team_points[self.team_id]
+
+        accounted_delta: int = 0
+        inferring_unit_info: List[Tuple[int, int, int]] = []  # (relic_idx, r_pos_x, r_pos_y)
+        for u_pos in self.obs.units_position[self.team_id, self.obs.units_mask[self.team_id]]:
+            # 确定unit周围的遗迹中心点
+            relic_dist = np.max(np.abs(self.relic_center - u_pos), axis=1)
+            relic_idx = int(np.argmin(relic_dist))  # 有多个最近中心点时, 数据记录在下标最小处
+            if relic_dist[relic_idx] > C.RELIC_SPREAD:
+                continue
+
+            # 在TRUE点上的unit记录accounted_delta
+            relative_pos = u_pos - self.relic_center[relic_idx] + C.RELIC_SPREAD
+            grid_info = self.relic_nodes[relic_idx, relative_pos[0], relative_pos[1]]
+            if grid_info == RelicInfo.REAL.value:
+                accounted_delta += 1
+            elif grid_info == RelicInfo.UNKNOWN.value:
+                inferring_unit_info.append((relic_idx, relative_pos[0], relative_pos[1]))
+
+        if not len(inferring_unit_info):
+            return False
+
+        # 对inferring_unit_info中的点进行推断
+        result = RelicInfo.UNKNOWN.value
+        if delta_pts - accounted_delta == 0:
+            # 所有REAL点都已确定, 其他位置的unit都推断为FAKE
+            result = RelicInfo.FAKE.value
+        elif delta_pts - accounted_delta == len(inferring_unit_info):
+            # 所有unit都在REAL点上
+            result = RelicInfo.REAL.value
+
+        # 标记推断结果
+        if result == RelicInfo.UNKNOWN.value:
+            return False
+        for relic_idx, r_pos_x, r_pos_y in inferring_unit_info:
+            self.relic_nodes[relic_idx, r_pos_x, r_pos_y] = result
+            flipped_pos = utils.flip_coord((r_pos_x, r_pos_y), size=C.RELIC_SPREAD*2+1)
+            self.relic_nodes[relic_idx ^ 1, flipped_pos[0], flipped_pos[1]] = result  # 对称地标记
+
+        self.logger.info(f"Relic nodes: \n{self.relic_nodes[0].T}")
+        return True
 
     @staticmethod
     def energy_weight_fn(energy: int, move_cost: int) -> float:
--- utils.py
+++ utils.py
@@ -13,11 +13,6 @@ else:
     Shape = Literal
     _NDArray = Tuple
 
-def flip_matrix(arr: np.ndarray) -> np.ndarray:
-    """根据游戏的对称方式, 对输入的方阵进行翻转"""
-    assert arr.shape[0] == arr.shape[1] and arr.ndim == 2, "arr must be a square matrix"
-    return np.rot90(np.rot90(arr).T, -1)
-
 class Constants:
     """游戏常量"""
 
@@ -25,6 +20,8 @@ class Constants:
     """游戏地图大小"""
     MAX_UNITS = 16
     """最大单位数量"""
+    RELIC_SPREAD = 2
+    """遗迹得分点相对遗迹中心点的最大单轴距离"""
 
     DIRECTIONS = np.array([
         [0, -1],  # 上  (游戏方向值1)
@@ -39,3 +36,19 @@ class Constants:
 
     INIT_UNIT_ENERGY = 100
     """单位的初始能量"""
+
+def flip_matrix(arr: np.ndarray) -> np.ndarray:
+    """根据游戏的对称方式, 对输入的方阵进行翻转"""
+    assert arr.shape[0] == arr.shape[1] and arr.ndim == 2, "arr must be a square matrix"
+    return np.rot90(np.rot90(arr).T, -1)
+
+flip_type = TypeVar("flip_type", Tuple[int, int], np.ndarray)
+def flip_coord(coord: flip_type, size: int = Constants.MAP_SIZE) -> flip_type:
+    """根据游戏的对称方式, 对输入的坐标进行翻转"""
+    if isinstance(coord, tuple):
+        x, y = coord
+        return (size - 1) - y, (size - 1) - x
+    else:
+        x = coord[..., 0]
+        y = coord[..., 1]
+        return np.stack([size - 1 - y, size - 1 - x], axis=-1)
